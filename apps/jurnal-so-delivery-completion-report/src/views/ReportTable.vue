66975edd8392ba62b4f7b806e4ceabdce659d7ad
Dimas Raka Septiawan <dimas.raka@mekari.com>
2022-08-16 13:56:26 +0700

    feat: update jurnal sodc

--- apps/jurnal-so-delivery-completion-report/src/views/ReportTable.vue
+++ apps/jurnal-so-delivery-completion-report/src/views/ReportTable.vue
@@ -1,22 +1,23 @@
 <template>
   <mp-box>
-    <mp-table-container overflow-x="auto">
+    <mp-table-container overflow-x="auto" max-height="500px">
       <mp-table :is-hoverable="false">
-        <mp-table-head>
+        <mp-table-head is-fixed :style="{ background: '#EDF0F2' }">
           <mp-table-row>
-            <mp-table-cell as="th" scope="col" width="141px"> Transaction </mp-table-cell>
+            <mp-table-cell as="th" scope="col" width="50px"> Transaction </mp-table-cell>
             <mp-table-cell as="th" scope="col" width="250px"> Product </mp-table-cell>
-            <mp-table-cell as="th" scope="col" width="250px"> Description </mp-table-cell>
-            <mp-table-cell as="th" scope="col" width="115px"> Total product </mp-table-cell>
-            <mp-table-cell as="th" scope="col" width="94px"> Delivered </mp-table-cell>
-            <mp-table-cell as="th" scope="col" width="149px"> Remaining </mp-table-cell>
+            <mp-table-cell as="th" scope="col" width="115px"> Total order </mp-table-cell>
+            <mp-table-cell as="th" scope="col" width="115px"> Unshipped </mp-table-cell>
+            <mp-table-cell as="th" scope="col" width="115px"> On shipping </mp-table-cell>
+            <mp-table-cell as="th" scope="col" width="94px"> Total shipped </mp-table-cell>
             <mp-table-cell as="th" scope="col" text-align="right" width="158px"> Unit price </mp-table-cell>
-            <mp-table-cell as="th" scope="col" text-align="right" width="158px"> Remaining billed </mp-table-cell>
-            <mp-table-cell as="th" scope="col" text-align="right" width="158px"> Total price </mp-table-cell>
+            <mp-table-cell as="th" scope="col" text-align="right" width="158px"> Total order amount </mp-table-cell>
+            <mp-table-cell as="th" scope="col" text-align="right" width="158px"> Billed remaining </mp-table-cell>
           </mp-table-row>
         </mp-table-head>
         <mp-table-body>
           <template v-for="(report, index) in reports">
+            <!-- Row header -->
             <mp-table-row
               :id="`header-${index}`"
               cursor="pointer"
@@ -24,31 +25,21 @@
               :key="report.id"
               role="group"
             >
-              <mp-table-cell
-                as="td"
-                scope="row"
-                bg="gray.50"
-                colspan="9"
-                :class="!report.isExpanded && 'pixel-border-gray-600'"
-                v-bind="{ ...styles.headerCell }"
-              >
+              <mp-table-cell as="td" scope="row" colspan="9" :class="!report.isExpanded && 'pixel-border-gray-600'">
                 <mp-box position="relative">
                   <mp-flex position="absolute" top="-3">
-                    <mp-icon
-                      name="caret-down"
-                      :transform="report.isExpanded ? 'rotate(-180deg)' : null"
-                      transition="transform 0.2s"
-                      transform-origin="center"
-                    />
+                    <mp-icon name="caret-right" :transform="report.isExpanded ? 'rotate(90deg)' : null" transition="transform 0.2s" transform-origin="center" />
                     <mp-text font-weight="semibold" ml="3"> {{ report.no }} - {{ report.pelanggan }} </mp-text>
                   </mp-flex>
                 </mp-box>
               </mp-table-cell>
             </mp-table-row>
 
+            <!-- Row content -->
             <template v-if="report.isExpanded">
+              <!-- Loading -->
               <mp-table-row :id="`loading-${index}`" v-if="report.isLoading" :key="`loading-${index}`">
-                <mp-table-cell as="td" scope="row" class="pixel-border-gray-600" colspan="9">
+                <mp-table-cell as="td" scope="row" class="pixel-border-gray-600" colspan="9" bg="gray.50">
                   <mp-flex>
                     <mp-spinner size="sm" />
                     <mp-text ml="3">Memuat transaksi...</mp-text>
@@ -56,8 +47,9 @@
                 </mp-table-cell>
               </mp-table-row>
 
+              <!-- Error -->
               <mp-table-row :id="`error-${index}`" v-if="!report.isLoading && report.isError" :key="`error-${index}`">
-                <mp-table-cell as="td" scope="row" class="pixel-border-gray-600" colspan="9">
+                <mp-table-cell as="td" scope="row" class="pixel-border-gray-600" colspan="9" bg="gray.50">
                   <mp-flex>
                     <mp-icon name="error" variant="duotone" />
                     <mp-text ml="3" mr="1">Gagal memuat transaksi. </mp-text>
@@ -66,6 +58,7 @@
                 </mp-table-cell>
               </mp-table-row>
 
+              <!-- Success -->
               <template v-if="!report.isLoading && !report.isError">
                 <mp-table-row
                   :id="`content-${index}`"
@@ -73,42 +66,103 @@
                   :key="`datas-${index}-${data}`"
                   :style="{ whiteSpace: 'normal' }"
                 >
-                  <mp-table-cell as="td" scope="row" vertical-align="top" :class="dataIndex === report.datas.length - 1 && 'pixel-border-gray-600'">
-                    01/06/2021
+                  <mp-table-cell
+                    as="td"
+                    scope="row"
+                    vertical-align="top"
+                    bg="gray.50"
+                    :class="dataIndex === report.datas.length - 1 && 'pixel-border-gray-600'"
+                  >
+                    <mp-text>01/06/2021</mp-text>
+                    <mp-text font-size="sm" is-link>SD 0001</mp-text>
                   </mp-table-cell>
-                  <mp-table-cell as="td" scope="row" vertical-align="top" :class="dataIndex === report.datas.length - 1 && 'pixel-border-gray-600'">
-                    <mp-box><mp-text is-link line-height="md">Mouse Logitech MK12345</mp-text></mp-box>
-                  </mp-table-cell>
-                  <mp-table-cell as="td" scope="row" vertical-align="top" :class="dataIndex === report.datas.length - 1 && 'pixel-border-gray-600'">
+                  <mp-table-cell
+                    as="td"
+                    scope="row"
+                    vertical-align="top"
+                    bg="gray.50"
+                    max-width="250px"
+                    :class="dataIndex === report.datas.length - 1 && 'pixel-border-gray-600'"
+                  >
                     <mp-box>
+                      <mp-text is-link line-height="md">Mouse Logitech MK12345</mp-text>
                       <mp-tooltip label="Beli buat stok mainan untuk penjualan dibulan agustus nanti." :id="`tooltip-${index}-${data}`" width="250px">
-                        <mp-text class="pixel-line-clamp-2" line-height="md"> Beli buat stok mainan untuk penjualan dibulan agustus nanti. </mp-text>
+                        <mp-text is-truncated line-height="md"> Beli buat stok mainan untuk penjualan dibulan agustus nanti. </mp-text>
                       </mp-tooltip>
                     </mp-box>
                   </mp-table-cell>
-                  <mp-table-cell as="td" scope="row" vertical-align="top" :class="dataIndex === report.datas.length - 1 && 'pixel-border-gray-600'">
-                    10
+                  <mp-table-cell
+                    as="td"
+                    scope="row"
+                    vertical-align="top"
+                    bg="gray.50"
+                    :class="dataIndex === report.datas.length - 1 && 'pixel-border-gray-600'"
+                  >
+                    10 Unit
+                    <mp-box> </mp-box>
                   </mp-table-cell>
-                  <mp-table-cell as="td" scope="row" vertical-align="top" :class="dataIndex === report.datas.length - 1 && 'pixel-border-gray-600'">
-                    2
+                  <mp-table-cell
+                    as="td"
+                    scope="row"
+                    vertical-align="top"
+                    bg="gray.50"
+                    :class="dataIndex === report.datas.length - 1 && 'pixel-border-gray-600'"
+                  >
+                    0
+                    <mp-box> </mp-box>
                   </mp-table-cell>
-                  <mp-table-cell as="td" scope="row" vertical-align="top" :class="dataIndex === report.datas.length - 1 && 'pixel-border-gray-600'">
-                    8
+                  <mp-table-cell
+                    as="td"
+                    scope="row"
+                    vertical-align="top"
+                    bg="gray.50"
+                    :class="dataIndex === report.datas.length - 1 && 'pixel-border-gray-600'"
+                  >
+                    0
                   </mp-table-cell>
-                  <mp-table-cell as="td" scope="row" vertical-align="top" :class="dataIndex === report.datas.length - 1 && 'pixel-border-gray-600'">
+                  <mp-table-cell
+                    as="td"
+                    scope="row"
+                    vertical-align="top"
+                    bg="gray.50"
+                    :class="dataIndex === report.datas.length - 1 && 'pixel-border-gray-600'"
+                  >
+                    <mp-text>0</mp-text>
+                    <mp-text font-size="sm" color="gray.600">(Shipment 0%)</mp-text>
+                  </mp-table-cell>
+                  <mp-table-cell
+                    as="td"
+                    scope="row"
+                    vertical-align="top"
+                    bg="gray.50"
+                    :class="dataIndex === report.datas.length - 1 && 'pixel-border-gray-600'"
+                  >
                     <mp-text text-align="right"> 1.000.000,00 </mp-text>
                   </mp-table-cell>
-                  <mp-table-cell as="td" scope="row" vertical-align="top" :class="dataIndex === report.datas.length - 1 && 'pixel-border-gray-600'">
+                  <mp-table-cell
+                    as="td"
+                    scope="row"
+                    vertical-align="top"
+                    bg="gray.50"
+                    :class="dataIndex === report.datas.length - 1 && 'pixel-border-gray-600'"
+                  >
                     <mp-text text-align="right"> 1.000.000,00 </mp-text>
                   </mp-table-cell>
-                  <mp-table-cell as="td" scope="row" vertical-align="top" :class="dataIndex === report.datas.length - 1 && 'pixel-border-gray-600'">
+                  <mp-table-cell
+                    as="td"
+                    scope="row"
+                    vertical-align="top"
+                    bg="gray.50"
+                    :class="dataIndex === report.datas.length - 1 && 'pixel-border-gray-600'"
+                  >
                     <mp-text text-align="right"> 1.000.000,00 </mp-text>
                   </mp-table-cell>
                 </mp-table-row>
               </template>
 
+              <!-- Load more -->
               <mp-table-row v-if="!report.isLoading && !report.isError && report.datas < 100" :key="`load-more-transaction-${index}`">
-                <mp-table-cell as="td" scope="row" class="pixel-border-gray-600" colspan="9">
+                <mp-table-cell as="td" scope="row" class="pixel-border-gray-600" colspan="9" bg="gray.50">
                   <mp-flex align="center" justify="center" width="full">
                     <mp-button v-if="!report.loadMoreLoading" variant="outline" @click="handleLoadMoreTransaction(index)">
                       Load other transactions {{ `${report.datas}/100` }}
@@ -252,13 +306,6 @@ export default {
       ],
       isLoading: false,
       datas: [],
-
-      // Styling
-      styles: {
-        headerCell: {
-          _groupHover: { bg: "background" },
-        },
-      },
     };
   },
   methods: {
@@ -301,13 +348,6 @@ export default {
 </script>
 
 <style scoped>
-.pixel-line-clamp-2 {
-  overflow: hidden;
-  display: -webkit-box;
-  -webkit-box-orient: vertical;
-  -webkit-line-clamp: 2;
-}
-
 .pixel-border-gray-600 {
   border-color: var(--colors-gray-100);
 }
