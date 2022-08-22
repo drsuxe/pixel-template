25f23690df31687d4038e7c1984160feeef6c69e
Dimas Raka Septiawan <dimas.raka@mekari.com>
2022-08-22 13:45:01 +0700

    feat: add jurnal coa

---
+++ apps/jurnal-chart-of-account/src/components/SplitButton.vue
@@ -0,0 +1,33 @@
+<template>
+  <mp-box display="flex">
+    <mp-button variant="solid" border-right-radius="0"> {{ label }} </mp-button>
+    <mp-popover id="popover-01">
+      <mp-popover-trigger>
+        <mp-button border="0px" border-left="1px" border-left-color="ice.50" border-left-radius="0" variant="solid" right-icon="caret-down"> </mp-button>
+      </mp-popover-trigger>
+      <mp-popover-content max-width="64" bg="white" rounded="md" shadow="lg" border-width="1px" border-color="gray.400">
+        <slot name="popoverContent" />
+      </mp-popover-content>
+    </mp-popover>
+  </mp-box>
+</template>
+
+<script>
+import { MpBox, MpButton, MpPopover, MpPopoverTrigger, MpPopoverContent } from "@mekari/pixel";
+export default {
+  props: {
+    label: {
+      type: String,
+      required: true,
+      default: "",
+    },
+  },
+  components: {
+    MpBox,
+    MpButton,
+    MpPopover,
+    MpPopoverTrigger,
+    MpPopoverContent,
+  },
+};
+</script>
